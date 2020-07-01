f63048d26744a1dda45ef25ea308ecbe06892c05
arshpunia <arshp98@my.yorku.ca>
2020-07-01 10:36:45 -0400

    Coin deposit mechanism in-place. Need to refactor some of the code

--- AddBlocks.py
+++ AddBlocks.py
@@ -1,4 +1,8 @@
 from hashlib import sha256
+import csv
+from dotenv import load_dotenv
+from datetime import datetime
+import os 
 """
 
 """
@@ -29,8 +33,44 @@ def proof_of_work(UserFile):
 def mine(ledger_file, hashcode):
     with open(ledger_file,'a+') as f:
         f.write(hashcode+"\n")
+    deposit_shc()
 
+def deposit_shc():
+    load_dotenv()
+    ap_acct = os.getenv('MINING_RECORDS_FILE')
+    t_date = datetime.now().strftime("%Y-%m-%d")
+    date_found = False
+    
+    if not os.path.isfile(ap_acct):
+        with open(ap_acct,'a') as acct:
+            print("Account file created")
+        acct.close()
+    
+    with open(ap_acct,"r+",newline='') as acct:
+        ##deposit_records = acct.readlines()
+        deposit_records = csv.reader(acct, delimiter = ',')
+        
+        for dates in deposit_records:
+            
+            if (t_date in dates):
+                date_found = True
+                print("t_date found")
+                with open(ap_acct,"r+",newline='') as acct_edit:
+                    account_writer = csv.writer(acct_edit, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                    dates[1] = int(dates[1])+1
+                    account_writer.writerow(dates)
+                    print("updated today's date")
+                    ##update_shc_account_records(t_date, deposit_records, acct_edit)
+                break
+            
+            
+        if date_found == False:
+            account_writer = csv.writer(acct, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            account_writer.writerow([t_date.strip('\n'),'1'.strip('\n')])
+            print("written today's date")
+    acct.close()
 
+   
 def count_balance(ledgerfile):
     line_count = 0
     with open(ledgerfile,'r') as f:
